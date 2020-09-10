676f6ade479df1f74fd879e58cc62283fe02a0e5
nervelin <2018185977@qq.com>
2020-09-10 20:11:46 +0800

    动态路由匹配

---
+++ VueDay06/product/index.js
@@ -0,0 +1,4 @@
+import { x } from "./m1.js"
+import m from "./m1.js"
+console.log(m);
+console.log(x);
---
+++ VueDay06/product/m1.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 100
+
+export default {
+    a,
+    b
+}
+// 按需导出
+export const x = 99
